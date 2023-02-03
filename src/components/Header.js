9519feebce262bce867c6cf63557fec900111a4d
Camry <106214553+CamryLE@users.noreply.github.com>
2023-02-03 09:59:32 -0500

    add component, begin styling

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+
+
+import Button from './Button'
+const Header = ({}) => {
+    return (
+
+    <header className='header'>
+        <h1 className='title'> TODO </h1>
+        <Button className='button' 
+        // {darkMode ? 'white' : 'black'} 
+        />
+    
+    </header>
+
+    )
+}
+
+export default Header
